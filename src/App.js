d8e8ac892f19d2f9e058100957e4cd5797ad9116
Edmund Situmorang <edmundsitumorang@gmail>
2021-04-12 15:18:07 +0700

    fully functional react todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { Button,FormControl, Input, InputLabel } from '@material-ui/core';
+import Todo from './Todo';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [ todos, setTodos ] = useState([ 'Contoh 1', 'contoh2' ]);
+	const [ input, setInput ] = useState('');
+	console.log('🚀', input);
+
+	const addTodo = (event) => {
+		// this will fire off when clicking submit button
+		event.preventDefault(); // stop page refress when clicking submit on FORM
+		console.log('Submitted');
+		setTodos([ ...todos, input ]);
+		setInput(''); // clear up the input after hitting submit
+	};
+
+	return (
+		<div className="App">
+			<h1>Hello World</h1>
+			<form>
+
+				<FormControl>
+					<InputLabel> Write A Todo</InputLabel>
+					<Input value={input} onChange={(event) => setInput(event.target.value)}/>
+				</FormControl>
+
+				<Button disabled={!input} type="submit" onClick={addTodo} variant="contained" color="primary">
+					Add Todo
+				</Button>
+				{/* <button type="submit" onClick={addTodo}>Add Todo</button> */}
+			</form>
+
+			<ul>{todos.map(todo => (
+				<Todo text={todo}/>
+			))}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
