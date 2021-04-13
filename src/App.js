d17909a4b05e75468db6dcbbd3e03971e3071b4d
Edmund Situmorang <edmund@situmorang.com>
2021-04-13 15:45:29 +0700

    fixed multiple className

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import { Button,FormControl, Input, InputLabel } from '@material-ui/core';
+import { Button,FormControl, Input, InputLabel, makeStyles, TextField } from '@material-ui/core';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import CloudOffIcon from '@material-ui/icons/CloudOff';
+import AccountCircle from '@material-ui/icons/AccountCircle';
 import Todo from './Todo';
 import db from './firebase';
 import firebase from 'firebase';
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+      maxWidth: 752,
+      backgroundColor: theme.palette.background.paper,
+	//   align:"center"
+	//   justifyItems:'center'
+	},
+  }));
 
 function App() {
 	const [ todos, setTodos ] = useState([]);
 	const [ input, setInput ] = useState('');
+	const [ username, setUsername] = useState('');
+	// const [ username, setInput ] = useState('');
+	const [checked, setChecked] = useState([1]);
+
+	const classes = useStyles();
 
 	//when the app loads, we need to listen to database and fetch new todos as they get added/removed
 	useEffect(() => {
 		db.collection('todos').orderBy('timestamp', 'asc').onSnapshot(snapshot => [
 			// console.log(snapshot.docs.map(doc => doc.data()))
-			setTodos(snapshot.docs.map(doc => ({id: doc.id, todo: doc.data().todo})))
+			setTodos(snapshot.docs.map(doc => ({
+				id: doc.id,
+				username: doc.data().username,
+				todo: doc.data().todo,
+				checked: doc.data().checked
+			})))
 		])
 	}, [])
 
@@ -24,31 +46,85 @@ function App() {
 
 		db.collection('todos').add({
 			todo: input,
+			username: username,
+			checked: false,
 			timestamp: firebase.firestore.FieldValue.serverTimestamp()
 		})
 		setInput(''); // clear up the input after hitting submit
+		setUsername('');
 	};
 
 	return (
-		<div className="App">
-			<h1>Situmorang Todo</h1>
+		// <div align="center" className="App">
+		<div align="center" className={`${App} ${classes.root}`}>
+			<h1 >CheckList</h1>
 			<form>
 
 				<FormControl>
-					<InputLabel> ✅ Click here to add</InputLabel>
-					<Input value={input} onChange={(event) => setInput(event.target.value)}/>
+					{/* <InputLabel variant="outlined" color='primary'> ✅  here to add</InputLabel>
+					<Input value={input} onChange={(event) => setInput(event.target.value)}/> */}
+					<TextField
+						id="outlined-full-width"
+						label="✅ ⦿here to add"
+						style={{ margin: 7 }}
+						// placeholder="Placeholder"
+						// helperText="Full width!"
+						// fullWidth
+						// margin="normal"
+						// InputLabelProps={{
+						// 	shrink: true,
+						// }}
+						multiline
+						variant="outlined"
+						value={input}
+						onChange={(event) => setInput(event.target.value)}
+					/>
+
+					{/* <InputLabel style={{ marginRight:50 }}>User</InputLabel> */}
+					<Input
+						id="input-with-icon-adornment"
+						placeholder="user name"
+						style={{ margin: 7 }}
+						// defaultValue="private"
+						// input={username}
+						value={username}
+						startAdornment={
+							<InputAdornment position="start">
+							<AccountCircle />
+							</InputAdornment>
+						}
+						onChange={(event) => setUsername(event.target.value)}
+       				/>
+
+					<Button 
+						disabled={!input} 
+						type="submit" 
+						onClick={addTodo} 
+						variant="contained" 
+						color="primary"
+					>
+						Add
+					</Button>
 				</FormControl>
 
-				<Button disabled={!input} type="submit" onClick={addTodo} variant="contained" color="primary">
-					Add Todo
-				</Button>
-				{/* <button type="submit" onClick={addTodo}>Add Todo</button> */}
+
 			</form>
 
-			<ul>{todos.map(todo => (
+			<ul 
+			// align="center"
+			// justify = "center"
+			// alignItems = "center"
+			// mx="auto" 
+			// style={{ margin: 7 }}
+			>{todos.map(todo => (
 				<Todo todo={todo}/>
 			))}
 			</ul>
+
+			{/* window.addEventListener('offline', function(e) {
+			console.log('offline')});
+
+			window.addEventListener('online', function(e) { console.log('online')}); */}
 		</div>
 	);
 }
--- src/Todo.js
+++ src/Todo.js
@@ -1,16 +1,69 @@
 import React from 'react';
-import {Button, List, ListItem, ListItemText} from '@material-ui/core';
+import {Checkbox, Grid, IconButton, List, ListItem, ListItemSecondaryAction, ListItemText, makeStyles} from '@material-ui/core';
 import db from './firebase';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import EditIcon from '@material-ui/icons/Edit';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+      maxWidth: 752,
+      backgroundColor: theme.palette.background.paper
+    },
+    demo: {
+      backgroundColor: theme.palette.background.paper
+    },
+    title: {
+      margin: theme.spacing(4, 0, 2),
+    },
+  }));
 
 function Todo(props) {
+    const classes = useStyles();
+    const [checked, setChecked] = React.useState([1]);
+
+    const handleToggle = (value) => () => {
+      const currentIndex = checked.indexOf(value);
+      const newChecked = [...checked];
+  
+      if (currentIndex === -1) {
+        newChecked.push(value);
+      } else {
+        newChecked.splice(currentIndex, 1);
+      }
+  
+      setChecked(newChecked);
+    };
     return (
-        <List>
-            <ListItem>
-             <ListItemText primary={props.todo.todo} secondary="🧑‍🦰‍ user" />
-            </ListItem>
-            <DeleteForeverIcon onClick={event => db.collection('todos').doc(props.todo.id).delete()}/>
-        </List>
+        <div lassName={classes.root}>
+
+            <List>
+                <Grid container>
+
+                    <ListItem>
+                        <Checkbox
+                            edge="start"
+                            // onChange={handleToggle(value)}
+                            // checked={checked.indexOf(value) !== -1}
+                            // inputProps={{ 'aria-labelledby': labelId }}
+                            // onClick={event => db.collection('todos').doc(props.todo.id)}
+                        />
+                        <ListItemText style={{ marginRight:50 }} primary={props.todo.todo} secondary={"🧑‍🦰 " + props.todo.username}/>
+                    </ListItem>
+                    <ListItemSecondaryAction>
+                        {/* <IconButton edge="end" aria-label="edit">
+                            <EditIcon />
+                        </IconButton> */}
+                        <IconButton
+                            // edge="end" 
+                            aria-label="delete">
+                            <DeleteForeverIcon onClick={event => db.collection('todos').doc(props.todo.id).delete()}/>
+                        </IconButton>
+                    </ListItemSecondaryAction>
+                </Grid>
+            </List>
+
+        </div>
     )
 }
 
